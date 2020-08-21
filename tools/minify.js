f67a4b29910707d4923779c079000f556d917744
Snazzah <suggesttosnazzy@gmail.com>
2020-08-21 17:56:25 -0500

    Use terser instead of javascript-minifier
    closes #1

--- tools/minify.js
+++ tools/minify.js
@@ -1,15 +1,14 @@
-const jsMinify = require("javascript-minifier");
+const { minify } = require("terser");
 const fs = require("fs");
 
-async function minify(code) {
+async function hackmudMinify(code) {
   const anon_code = Date.now().toString(16);
-	return (await jsMinify.minify(
-		code
-			.replace("function", `function script_${anon_code}`)
-			.replace(/#(?:(?:f|h|m|l|n|[0-4])?s|db|G|FMCL)/g, a => {
-				return a.replace("#", `_hash_${anon_code}_`);
-			})
-	))
+	return (await minify(code
+		.replace("function", `function script_${anon_code}`)
+		.replace(/#(?:(?:f|h|m|l|n|[0-4])?s|db|G|FMCL)/g, a => {
+			return a.replace("#", `_hash_${anon_code}_`);
+		})
+	)).code
 		.replace(`script_${anon_code}`, "")
 		.replace(new RegExp(`_hash_${anon_code}_`, 'g'), a => {
 			return '#';
@@ -33,7 +32,7 @@ const files = fs.readdirSync('./dist')
 
 files.map(async file => {
 	const code = fs.readFileSync(`./dist/${file}`, { encoding: 'utf8' });
-	const minCode = await minify(code);
+	const minCode = await hackmudMinify(code);
 	fs.writeFileSync(`./dist/${file}`, addAutocomplete(file, minCode));
 	console.log(`${file} [${minCode.length} chars]`)
 })
