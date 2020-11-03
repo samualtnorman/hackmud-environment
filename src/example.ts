c9943c683262199d08858ad4578a961c30ad7120
Samual Norman <samualtnorman@gmail.com>
2020-11-03 16:01:20 +0000

    Update example.ts

--- src/example.ts
+++ src/example.ts
@@ -1,5 +1,5 @@
 export function script(context: Context, args?: null | Record<string, unknown>) {
 	var caller = context.caller
-	var l = #fs.scripts.lib()
+	var l = $fs.scripts.lib()
 	return { ok:false }
 }
